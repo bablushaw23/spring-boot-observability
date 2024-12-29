975fcc28c5ecfa393dd842c7c2c6eae43aa7c933
bablushaw23 <46132062+bablushaw23@users.noreply.github.com>
2024-12-29 21:45:34 +0530

    intercepting request and response header and body in jaeger

--- order-service/src/main/java/com/example/observability/order/OrderApplication.java
+++ order-service/src/main/java/com/example/observability/order/OrderApplication.java
@@ -1,10 +1,16 @@
 package com.example.observability.order;
 
+import io.opentelemetry.api.trace.Span;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class OrderApplication {
@@ -14,7 +20,34 @@ public class OrderApplication {
 
     @Bean
     RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+        return builder.additionalInterceptors(new TracingRestTemplateInterceptor()).build();
+    }
+
+   class TracingRestTemplateInterceptor implements ClientHttpRequestInterceptor {
+
+        @Override
+        public ClientHttpResponse intercept(org.springframework.http.HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+            Span currentSpan = Span.current();
+            if (currentSpan != null) {
+                // Add request headers
+                request.getHeaders().forEach((key, value) ->
+                        currentSpan.setAttribute("http.request.header." + key, String.join(",", value))
+                );
+                // Add request body
+                currentSpan.setAttribute("http.request.body", new String(body));
+            }
+
+            ClientHttpResponse response = execution.execute(request, body);
+
+            if (currentSpan != null) {
+                // Add response headers
+                response.getHeaders().forEach((key, value) ->
+                        currentSpan.setAttribute("http.response.header." + key, String.join(",", value))
+                );
+            }
+
+            return response;
+        }
     }
 
 }
